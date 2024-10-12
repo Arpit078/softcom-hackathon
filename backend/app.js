f004a9a8595618725edd153534a5892ce3d6dda5
Arpit078 <verma.arpit078@gmail.com>
2024-10-13 05:15:23 +0530

    dasd

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,23 @@ const cors = require('cors');
 
 dotenv.config();
 const app = express();
+const port = 3000;
+const performSearch = require("./worker/search.js")
+const scrapeUrls = require("./worker/scraper.js")
+const correctGrammar = require("./worker/summariser.js")
+// Define a route
+app.post('/generate_data', async (req, res) => { // handles user 
+  let query = req.query;
+  let urls = await performSearch(query);
+  let resultObj = await scrapeUrls(urls);
+  let correct = await correctGrammar(resultObj);
+  res.send({data : JSON.parse(correct)});
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 // Middleware
 app.use(cors());
