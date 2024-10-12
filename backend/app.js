df7f2625fc24a6e93cfaaf64225de7d0b1c8a32f
Arpit078 <verma.arpit078@gmail.com>
2024-10-13 02:55:47 +0530

    required routes

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,16 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
-
+/*
+ 1. handles user queries.
+        - user query ko queue
+        - db me status update report ki generating. unique id : status
+        - cron scrape karti hai 
+        - update db to generated.
+ 2. hanles data for generated page.
+        - db se unique id ka data nikalke json me return.
+3.  handle post requests jo db me update karengi.
+*/
 // Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
